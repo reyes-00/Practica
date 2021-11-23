e7856fd6a55f55dbfa4d11e87dcf6da7a624f41f
Your Name <you@example.com>
2021-11-23 16:53:12 -0600

    cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 // create express app
 const app = express();
-
+app.use(cors());
 // Setup server port
 const port = process.env.PORT || 5000;
 
@@ -23,6 +24,7 @@ const employeeRoutes = require('./src/routes/employee.routes')
 
 // using as middleware
 app.use('/api/v1/employees', employeeRoutes)
+app.use('/api/v2/employees', employeeAllRoutes)
 
 // listen for requests
 app.listen(port, () => {
